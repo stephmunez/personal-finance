895a5c255d96c4629fdcf9d2a624ac22ca301099
Stephen Muñez <sjtmunez@gmail.com>
2024-09-20 13:10:31 +0800

    feat: transaction api routes

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -2,7 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('this is the transactions route');
+  res.send('Get all transactions');
+});
+
+router.get('/search', (req, res) => {
+  res.send('Search and filter transactions');
+});
+
+router.post('/', (req, res) => {
+  res.send('Create a new transaction');
+});
+
+router.patch('/:id', (req, res) => {
+  res.send('Update an existing transaction');
+});
+
+router.delete('/:id', (req, res) => {
+  res.send('Delete a transaction');
 });
 
 module.exports = router;
