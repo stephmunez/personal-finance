ce6f161edbab940261cd9b060696242faedb797a
Stephen Muñez <sjtmunez@gmail.com>
2024-09-22 07:05:37 +0800

    feat: add timestamps to models

--- backend/models/Balance.js
+++ backend/models/Balance.js
@@ -1,20 +1,23 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const balanceSchema = new Schema({
-  current: {
-    type: Number,
-    required: true,
+const balanceSchema = new Schema(
+  {
+    current: {
+      type: Number,
+      required: true,
+    },
+    income: {
+      type: Number,
+      required: true,
+    },
+    expenses: {
+      type: Number,
+      required: true,
+    },
   },
-  income: {
-    type: Number,
-    required: true,
-  },
-  expenses: {
-    type: Number,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Balance = mongoose.model('Balance', balanceSchema);
 module.exports = Balance;
--- backend/models/Budget.js
+++ backend/models/Budget.js
@@ -1,20 +1,23 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const budgetSchema = new Schema({
-  category: {
-    type: String,
-    required: true,
+const budgetSchema = new Schema(
+  {
+    category: {
+      type: String,
+      required: true,
+    },
+    maximum: {
+      type: Number,
+      required: true,
+    },
+    theme: {
+      type: String,
+      required: true,
+    },
   },
-  maximum: {
-    type: Number,
-    required: true,
-  },
-  theme: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Budget = mongoose.model('Budget', budgetSchema);
 module.exports = Budget;
