6dc17135e42075f8c960fdd3c1510a601912d1c9
Stephen Muñez <sjtmunez@gmail.com>
2024-09-23 14:40:59 +0800

    Update overview.js

--- backend/routes/overview.js
+++ backend/routes/overview.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { getOverview } = require('../controllers/overview');
 
-router.get('/', (req, res) => {
-  res.send('this is the overview route');
-});
+router.get('/', getOverview);
 
 module.exports = router;
