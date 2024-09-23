7993ce4522755e552a52b657dc2aee16af4bdf2d
Stephen Muñez <sjtmunez@gmail.com>
2024-09-23 16:20:37 +0800

    refactor: get transactions

--- backend/controllers/transactions.js
+++ backend/controllers/transactions.js
@@ -3,14 +3,8 @@ const Transaction = require('../models/Transaction');
 const { StatusCodes } = require('http-status-codes');
 
 const getTransactions = async (req, res) => {
-  try {
-    const transactions = await Transaction.find({});
-    res
-      .status(StatusCodes.OK)
-      .send({ transactions, count: transactions.length });
-  } catch (error) {
-    res.status(StatusCodes.BAD_REQUEST).send({ message: error.message });
-  }
+  const transactions = await Transaction.find({}).sort({ createdAt: -1 });
+  res.status(StatusCodes.OK).send({ transactions, count: transactions.length });
 };
 
 const getTransaction = async (req, res) => {
