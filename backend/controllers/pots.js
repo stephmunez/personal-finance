df5d5cb3d2a97f306885eb81b9b462276af28a5f
Stephen Muñez <sjtmunez@gmail.com>
2024-09-24 11:00:18 +0800

    feat: pots controller

---
+++ backend/controllers/pots.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const Pot = require('../models/Pot');
+const { StatusCodes } = require('http-status-codes');
+
+const getPots = async (req, res) => {
+  const pots = await Pot.find({}).sort({ createdAt: -1 });
+  res.status(StatusCodes.OK).send({ pots, count: pots.length });
+};
+
+const getPot = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .send({ error: `No pot with id ${id}` });
+  }
+
+  const pot = await Pot.findOne({ _id: id });
+
+  if (!pot) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .send({ error: `No job with id ${id}` });
+  }
+
+  res.status(StatusCodes.OK).send(pot);
+};
+
+const createPot = async (req, res) => {
+  try {
+    const pot = await Pot.create(req.body);
+    res.status(StatusCodes.OK).send(pot);
+  } catch (error) {
+    res.status(StatusCodes.BAD_REQUEST).send({ error: error.message });
+  }
+};
+
+const updatePot = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const pot = await Pot.findOneAndUpdate({ _id: id }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(StatusCodes.OK).send({ pot });
+  } catch (error) {
+    res.status(StatusCodes.BAD_REQUEST).send({ error: error.message });
+  }
+};
+
+const deletePot = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const pot = await Pot.findOneAndDelete({
+      _id: id,
+    });
+    res.status(StatusCodes.OK).send();
+  } catch (error) {
+    res.status(StatusCodes.BAD_REQUEST).send({ error: error.message });
+  }
+};
+
+module.exports = {
+  getPots,
+  getPot,
+  createPot,
+  updatePot,
+  deletePot,
+};
--- backend/routes/pots.js
+++ backend/routes/pots.js
@@ -1,46 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const Pot = require('../models/Pot');
-const { StatusCodes } = require('http-status-codes');
+const {
+  getPots,
+  getPot,
+  createPot,
+  updatePot,
+  deletePot,
+} = require('../controllers/pots');
 
-// Get all pots
-router.get('/', (req, res) => {
-  res.send('Get all pots');
-});
-
-// Get a specific pot
-router.get('/:id', (req, res) => {
-  res.send('Get a specific pot');
-});
-
-// Create a new pot
-router.post('/', async (req, res) => {
-  try {
-    const pot = await Pot.create(req.body);
-    res.status(StatusCodes.OK).send(pot);
-  } catch (error) {
-    res.status(StatusCodes.BAD_REQUEST).send({ error: error.message });
-  }
-});
-
-// Add money to a pot
-router.patch('/:id/add', (req, res) => {
-  res.send('Add money to a pot');
-});
-
-// Withdraw money from a pot
-router.patch('/:id/withdraw', (req, res) => {
-  res.send('Withdraw money from a pot');
-});
-
-// Update a pot (e.g. change its name or goal)
-router.patch('/:id', (req, res) => {
-  res.send('Update a pot');
-});
-
-// Delete a pot
-router.delete('/:id', (req, res) => {
-  res.send('Delete a pot');
-});
+router.get('/', getPots);
+router.get('/:id', getPot);
+router.post('/', createPot);
+router.patch('/:id', updatePot);
+router.delete('/:id', deletePot);
 
 module.exports = router;
