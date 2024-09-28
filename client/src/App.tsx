4588a8b923455e46e69225e7ecd17663e91a2a2c
Stephen Muñez <sjtmunez@gmail.com>
2024-09-29 04:54:13 +0800

    feat: budgets route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,23 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Home from './routes/Home';
-import Root from './routes/root';
-import Transactions from './routes/Transactions';
+import Budgets from "./routes/Budgets";
+import Home from "./routes/Home";
+import Root from "./routes/root";
+import Transactions from "./routes/Transactions";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Root />,
     children: [
       { index: true, element: <Home /> },
       {
-        path: '/transactions',
+        path: "/transactions",
         element: <Transactions />,
+      },
+      {
+        path: "/budgets",
+        element: <Budgets />,
       },
     ],
   },
