e2044f307e7733d9b8b7a9d391a74b97d03f20b9
Stephen Muñez <sjtmunez@gmail.com>
2024-11-02 11:24:41 +0800

    feat: restrict pot creation to available themes

--- server/models/Pot.js
+++ server/models/Pot.js
@@ -1,6 +1,24 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+const themes = [
+  '#277C78',
+  '#F2CDAC',
+  '#82C9D7',
+  '#626070',
+  '#C94736',
+  '#826CB0',
+  '#597C7C',
+  '#93674F',
+  '#934F6F',
+  '#3F82B2',
+  '#97A0AC',
+  '#7F9161',
+  '#AF81BA',
+  '#CAB361',
+  '#BE6C49',
+];
+
 const potSchema = new Schema(
   {
     name: {
@@ -18,27 +36,21 @@ const potSchema = new Schema(
     theme: {
       type: String,
       required: true,
-      enum: [
-        '#277C78',
-        '#F2CDAC',
-        '#82C9D7',
-        '#626070',
-        '#C94736',
-        '#826CB0',
-        '#597C7C',
-        '#93674F',
-        '#934F6F',
-        '#3F82B2',
-        '#97A0AC',
-        '#7F9161',
-        '#AF81BA',
-        '#CAB361',
-        '#BE6C49',
-      ],
+      enum: themes,
     },
   },
   { timestamps: true }
 );
+
+potSchema.pre('save', async function (next) {
+  const potCount = await Pot.countDocuments();
+  if (potCount >= themes.length) {
+    const error = new Error(`All themes are used`);
+    error.statusCode = 400;
+    return next(error);
+  }
+  next();
+});
 
 const Pot = mongoose.model('Pot', potSchema);
 module.exports = Pot;
