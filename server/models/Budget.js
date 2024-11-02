3a68f40b4905b6e3a0b39c25e6793ca824391c74
Stephen Muñez <sjtmunez@gmail.com>
2024-11-02 11:24:57 +0800

    feat: restrict budget creation to available categories

--- server/models/Budget.js
+++ server/models/Budget.js
@@ -1,23 +1,25 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+const categories = [
+  'Entertainment',
+  'Bills',
+  'Groceries',
+  'Dining Out',
+  'Transportation',
+  'Personal Care',
+  'Education',
+  'Lifestyle',
+  'Shopping',
+  'General',
+];
+
 const budgetSchema = new Schema(
   {
     category: {
       type: String,
       required: true,
-      enum: [
-        'Entertainment',
-        'Bills',
-        'Groceries',
-        'Dining Out',
-        'Transportation',
-        'Personal Care',
-        'Education',
-        'Lifestyle',
-        'Shopping',
-        'General',
-      ],
+      enum: categories,
     },
     maximum: {
       type: Number,
@@ -26,6 +28,16 @@ const budgetSchema = new Schema(
   },
   { timestamps: true }
 );
+
+budgetSchema.pre('save', async function (next) {
+  const budgetCount = await Budget.countDocuments();
+  if (budgetCount >= categories.length) {
+    const error = new Error(`All categories are used`);
+    error.statusCode = 400;
+    return next(error);
+  }
+  next();
+});
 
 const Budget = mongoose.model('Budget', budgetSchema);
 module.exports = Budget;
