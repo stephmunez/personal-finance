9abd95102dc245b440715f045d880b83fd8374bf
Stephen Muñez <sjtmunez@gmail.com>
2024-11-02 11:20:12 +0800

    feat: restrict theme values in pot schema

--- server/models/Pot.js
+++ server/models/Pot.js
@@ -18,6 +18,23 @@ const potSchema = new Schema(
     theme: {
       type: String,
       required: true,
+      enum: [
+        '#277C78',
+        '#F2CDAC',
+        '#82C9D7',
+        '#626070',
+        '#C94736',
+        '#826CB0',
+        '#597C7C',
+        '#93674F',
+        '#934F6F',
+        '#3F82B2',
+        '#97A0AC',
+        '#7F9161',
+        '#AF81BA',
+        '#CAB361',
+        '#BE6C49',
+      ],
     },
   },
   { timestamps: true }
