cc7fbe57340e87e0a00e384142d240cd023a3094
Stephen Muñez <sjtmunez@gmail.com>
2024-11-13 15:51:01 +0800

    feat: overview pots tablet view

--- client/src/components/OverviewPots/index.tsx
+++ client/src/components/OverviewPots/index.tsx
@@ -31,7 +31,7 @@ const OverviewPots = ({ pots }: OverviewPotsProps) => {
   }, [pots]);
 
   return (
-    <section className="flex flex-col gap-5 rounded-xl bg-white px-5 py-6">
+    <section className="flex flex-col gap-5 rounded-xl bg-white px-5 py-6 md:p-8">
       <div className="flex w-full items-center justify-between">
         <h2 className="text-xl font-bold leading-[1.2] text-grey-900">Pots</h2>
         <Link
@@ -41,56 +41,61 @@ const OverviewPots = ({ pots }: OverviewPotsProps) => {
           See Details <img src={iconCaretRight} alt="caret right icon" />
         </Link>
       </div>
-      <div className="flex w-full flex-col gap-5 rounded-xl bg-beige-100 p-4">
-        <div className="flex items-center gap-4">
-          <div className="flex h-10 w-10 items-center justify-center">
-            <img src={iconPot} alt="pot icon" />
-          </div>
-          <div className="flex flex-col gap-2">
-            <h3 className="text-sm leading-normal text-grey-500">
-              Total Saved
-            </h3>
-            {loading ? (
-              <div className="h-10 w-32 animate-pulse rounded bg-grey-100"></div>
-            ) : (
-              <span className="text-[2rem] font-bold leading-[1.2] tracking-normal text-grey-900">
-                P{totalSaved.toFixed(0)}
-              </span>
-            )}
+      <div className="flex w-full flex-col gap-5 md:flex-row">
+        <div className="flex w-full flex-col gap-5 rounded-xl bg-beige-100 p-4 md:w-2/5">
+          <div className="flex items-center gap-4">
+            <div className="flex h-10 w-10 items-center justify-center">
+              <img src={iconPot} alt="pot icon" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <h3 className="text-sm leading-normal text-grey-500">
+                Total Saved
+              </h3>
+              {loading ? (
+                <div className="h-10 w-32 animate-pulse rounded bg-grey-100"></div>
+              ) : (
+                <span className="text-[2rem] font-bold leading-[1.2] tracking-normal text-grey-900">
+                  P{totalSaved.toFixed(0)}
+                </span>
+              )}
+            </div>
           </div>
         </div>
+        <ul className="flex w-full flex-wrap gap-4 md:w-3/5">
+          {loading
+            ? Array.from({ length: 4 }).map((_, index) => (
+                <li
+                  key={index}
+                  className="flex flex-[1_1_calc(50%-16px)] gap-4"
+                >
+                  <div className="bg-grey-200 h-full w-1 animate-pulse rounded-lg"></div>
+                  <div className="flex flex-col gap-1">
+                    <div className="h-4 w-20 animate-pulse rounded bg-grey-100"></div>
+                    <div className="h-5 w-16 animate-pulse rounded bg-grey-100"></div>
+                  </div>
+                </li>
+              ))
+            : topPots.map((pot) => (
+                <li
+                  key={pot._id}
+                  className="flex flex-[1_1_calc(50%-16px)] gap-4"
+                >
+                  <div
+                    className="h-full w-1 rounded-lg"
+                    style={{ backgroundColor: pot.theme }}
+                  ></div>
+                  <div className="flex flex-col gap-1">
+                    <h4 className="text-xs leading-normal text-grey-500">
+                      {pot.name}
+                    </h4>
+                    <span className="text-sm font-bold leading-normal text-grey-900">
+                      P{pot.total.toFixed(0)}
+                    </span>
+                  </div>
+                </li>
+              ))}
+        </ul>
       </div>
-      <ul className="flex w-full flex-wrap gap-4">
-        {loading
-          ? Array.from({ length: 4 }).map((_, index) => (
-              <li key={index} className="flex flex-[1_1_calc(50%-16px)] gap-4">
-                <div className="bg-grey-200 h-full w-1 animate-pulse rounded-lg"></div>
-                <div className="flex flex-col gap-1">
-                  <div className="h-4 w-20 animate-pulse rounded bg-grey-100"></div>
-                  <div className="h-5 w-16 animate-pulse rounded bg-grey-100"></div>
-                </div>
-              </li>
-            ))
-          : topPots.map((pot) => (
-              <li
-                key={pot._id}
-                className="flex flex-[1_1_calc(50%-16px)] gap-4"
-              >
-                <div
-                  className="h-full w-1 rounded-lg"
-                  style={{ backgroundColor: pot.theme }}
-                ></div>
-                <div className="flex flex-col gap-1">
-                  <h4 className="text-xs leading-normal text-grey-500">
-                    {pot.name}
-                  </h4>
-                  <span className="text-sm font-bold leading-normal text-grey-900">
-                    P{pot.total.toFixed(0)}
-                  </span>
-                </div>
-              </li>
-            ))}
-      </ul>
     </section>
   );
 };
