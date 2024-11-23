1aea9bae132be83012b34e0b61563699209c9c05
Stephen Muñez <sjtmunez@gmail.com>
2024-11-23 16:26:03 +0800

    feat: signing up and password hashing

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const { Schema } = mongoose;
 
 const userSchema = new Schema(
@@ -23,6 +24,32 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+// static sign up method
+userSchema.statics.signUp = async function (
+  firstName,
+  lastName,
+  email,
+  password
+) {
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error('Email already in use.');
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({
+    firstName,
+    lastName,
+    email,
+    password: hash,
+  });
+
+  return user;
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
