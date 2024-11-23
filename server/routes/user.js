6599eda18517267c6bf5921fa8a4c4e4b407fb15
Stephen Muñez <sjtmunez@gmail.com>
2024-11-23 15:30:36 +0800

    feat: user routes

---
+++ server/routes/user.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// login
+router.post('/login', () => {});
+
+// sign up
+router.post('/sign-up', () => {});
+
+module.exports = router;
