20d5b9eea818797545725cc1f3a5b15c5261d4f4
Stephen Muñez <sjtmunez@gmail.com>
2024-11-26 10:07:14 +0800

    feat: login loading state and server error

--- client/src/routes/Login/index.tsx
+++ client/src/routes/Login/index.tsx
@@ -1,5 +1,6 @@
 import { FormEvent, useState } from "react";
 import { Link } from "react-router-dom";
+import iconError from "../../assets/images/icon-bill-due.svg";
 import iconHidePassword from "../../assets/images/icon-hide-password.svg";
 import iconShowPassword from "../../assets/images/icon-show-password.svg";
 import logoLarge from "../../assets/images/logo-large.svg";
@@ -10,6 +11,8 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [serverError, setServerError] = useState("");
 
   const [errors, setErrors] = useState({
     email: "",
@@ -19,6 +22,10 @@ const Login = () => {
   const { user, setUser } = useAuthContext();
 
   const loginUser = async (user: User) => {
+    setIsLoading(true);
+    setErrors({ email: "", password: "" });
+    setServerError("");
+
     try {
       const response = await fetch("http://localhost:4000/api/v1/user/login", {
         method: "POST",
@@ -32,6 +39,9 @@ const Login = () => {
         setUser(data.user);
         localStorage.setItem("user", JSON.stringify(data.user));
         localStorage.setItem("token", JSON.stringify(data.token));
+      } else {
+        setIsLoading(false);
+        setServerError(data.error);
       }
     } catch (error) {
       console.error(
@@ -68,9 +78,12 @@ const Login = () => {
     if (validateForm()) {
       await loginUser({ email, password });
 
-      setEmail("");
-      setPassword("");
+      if (!serverError) {
+        setEmail("");
+        setPassword("");
+      }
       setErrors({ email: "", password: "" });
+      setIsLoading(false);
     }
   };
 
@@ -148,9 +161,17 @@ const Login = () => {
           <button
             type="submit"
             className="flex items-center justify-center rounded-lg bg-grey-900 py-4 text-sm font-bold leading-normal text-white disabled:cursor-not-allowed disabled:opacity-50"
+            disabled={isLoading}
           >
             Login
           </button>
+          {serverError && (
+            <div className="flex w-full items-center gap-2 rounded-lg border border-solid border-red bg-red/10 px-5 py-3 text-sm leading-normal text-red">
+              <img src={iconError} alt="error icon" />
+              <span>{serverError}</span>
+            </div>
+          )}
+
           <div className="flex items-center justify-center gap-2">
             <span className="text-sm leading-normal text-grey-500">
               Need to create a new account?
