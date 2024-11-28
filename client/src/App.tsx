cb010a514a82300df04fa9a504d626475c1e1df0
Stephen Muñez <sjtmunez@gmail.com>
2024-11-28 11:37:09 +0800

    feat: protect routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,9 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 import Budgets from "./routes/Budgets";
 import Home from "./routes/Home";
 import Login from "./routes/Login";
@@ -9,41 +13,43 @@ import Root from "./routes/root";
 import SignUp from "./routes/SignUp";
 import Transactions from "./routes/Transactions";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      { index: true, element: <Home /> },
-      {
-        path: "/transactions",
-        element: <Transactions />,
-      },
-      {
-        path: "/budgets",
-        element: <Budgets />,
-      },
-      {
-        path: "/pots",
-        element: <Pots />,
-      },
-      {
-        path: "/recurring-bills",
-        element: <RecurringBills />,
-      },
-      {
-        path: "/sign-up",
-        element: <SignUp />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-    ],
-  },
-]);
-
 const App = () => {
+  const { user } = useAuthContext();
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        { index: true, element: user ? <Home /> : <Navigate to="/login" /> },
+        {
+          path: "/transactions",
+          element: <Transactions />,
+        },
+        {
+          path: "/budgets",
+          element: user ? <Budgets /> : <Navigate to="login" />,
+        },
+        {
+          path: "/pots",
+          element: user ? <Pots /> : <Navigate to="login" />,
+        },
+        {
+          path: "/recurring-bills",
+          element: user ? <RecurringBills /> : <Navigate to="login" />,
+        },
+        {
+          path: "/sign-up",
+          element: !user ? <SignUp /> : <Home />,
+        },
+        {
+          path: "/login",
+          element: !user ? <Login /> : <Home />,
+        },
+      ],
+    },
+  ]);
+
   return <RouterProvider router={router} />;
 };
 
